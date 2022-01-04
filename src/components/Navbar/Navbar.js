9345d782c6510cfcd930e0473bab545c941a4cff
pablopsdigital <pablopsdigital@gmail.com>
2022-01-04 09:11:48 +0100

    icon problem deploy

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import Brand from '../../resources/svg/gomyecotrip-symbol.svg';
 import {Link} from 'react-router-dom';
 
 // Icons
-import iconSetGomyectrip from '../../resources/icons/icon-set-gomyectrip/selection.json';
-import IcomoonReact, {iconList} from 'icomoon-react';
+// import iconSetGomyectrip from '../../resources/icons/icon-set-gomyectrip/selection.json';
+// import IcomoonReact, {iconList} from 'icomoon-react';
 
 //Components
 import SearchAvailability from './SearchAvailability/SearchAvailability';
@@ -71,19 +71,20 @@ export default function Navbar() {
             <MenuLink to="/host/welcome">offers an experience</MenuLink>
           )}
           <Avatar onClick={() => setModalOpen(true)}>
-            <IcomoonReact
+            {/* <IcomoonReact
               iconSet={iconSetGomyectrip}
               color="#671cc9"
               size={25}
               icon="align-justify"
-            />
+            /> */}
 
             {userInfo ? (
               <div className="avatar">
                 <img src={userInfo.imageAvatar} />
               </div>
             ) : (
-              <IcomoonReact iconSet={iconSetGomyectrip} color="#671cc9" size={25} icon="user" />
+              <div></div>
+              // <IcomoonReact iconSet={iconSetGomyectrip} color="#671cc9" size={25} icon="user" />
             )}
           </Avatar>
 

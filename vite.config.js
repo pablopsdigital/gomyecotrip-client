e6c843101b1d8375ccd699f8eeddadc633216ec2
pablopsdigital <pablopsdigital@gmail.com>
2023-09-25 14:45:46 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@
 //   plugins: [react()]
 // });
 
-// import react from '@vitejs/plugin-react';
-// import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 
-// export default defineConfig(() => {
-//   return {
-//     build: {
-//       outDir: 'build'
-//     },
-//     plugins: [react()]
-//   };
-// });
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build'
+    },
+    plugins: [react()]
+  };
+});

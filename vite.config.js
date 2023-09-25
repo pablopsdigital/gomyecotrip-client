007c72fc88b3b9ae6eea4d8797b2b01ae98ab6ab
pablopsdigital <pablopsdigital@gmail.com>
2023-09-25 14:35:52 +0100

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import {defineConfig} from 'vitest/config';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// });
